ba09ac6b8f4ae11db8aea73d7523569c0cbb9ec9
gitraya <mail.raya15@gmail.com>
2026-07-08 00:23:40 +0700

    feat: add Dockerfile for Next.js web app and configure standalone output in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   turbopack: {
     root: dirname(fileURLToPath(import.meta.url)),
   },
